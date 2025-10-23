d750f901000a3c1f949e56d51f6e0d16b0e1d85c
Cassandra1801 <cassandramichaelsen@gmail.com>
2025-10-23 12:56:32 +0200

    H

---
+++ src/Pizza.java
@@ -0,0 +1,18 @@
+public class Pizza {
+
+    public Pizza(){}
+
+    private int pizzaID;
+    private String navn;
+    String toppings;
+    double pris;
+
+    public Pizza (int pizzaID, String navn, String toppings, double pris){
+
+        this.pizzaID = pizzaID;
+        this.navn = navn;
+        this.toppings = toppings;
+        this.pris = pris;
+    }
+
+}
