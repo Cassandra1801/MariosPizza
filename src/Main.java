90a420ee60b05ef1f7e74f40876730b5b76ec9b6
MariamGh2 <magh0004@stud.ek.dk>
2025-10-23 14:03:46 +0200

    jnfvldf

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 
 
 public class Main {
     public static boolean aabent = true;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        while (aabent == true )  {
+        while (aabent == true) {
 
-            List<Pizza> pizza =  new ArrayList<Pizza>();
+            List<Pizza> pizza = new ArrayList<Pizza>();
             pizza = FileUtil.readPizzaFromFile();
 
             for (Pizza p : pizza) {
-            System.out.println();
+                System.out.println(p);
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+                while (true) {
+
+                    LocalTime now = LocalTime.now();
+                    System.out.print("\rKlokken er: " + now.format(formatter)); // \r overskriver linjen
+                    Thread.sleep(1000); // venter 1 sekund før næste opdatering
+                }
             }
         }
     }
